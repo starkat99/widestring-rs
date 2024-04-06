5ef67a7de52f5fe59e0bf70b1bacdc3129f4a532
Kathryn Long <squeeself@gmail.com>
2024-04-06 17:08:50 -0400

    Fix clippy warning

--- src/lib.rs
+++ src/lib.rs
@@ -220,6 +220,7 @@
     trivial_numeric_casts,
     future_incompatible
 )]
+#![allow(renamed_and_removed_lints)] // Until min version gets bumped to after rename
 #![cfg_attr(not(feature = "std"), no_std)]
 #![doc(html_root_url = "https://docs.rs/widestring/1.0.2")]
 #![doc(test(attr(deny(warnings), allow(unused))))]
